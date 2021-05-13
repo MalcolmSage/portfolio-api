7eb9cf7f1b5d3d913413ae3d669f53904410aa0d
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-05-13 11:29:19 -0600

    added static root

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import environ 
+import os
 
 env = environ.Env()
 
@@ -160,6 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
